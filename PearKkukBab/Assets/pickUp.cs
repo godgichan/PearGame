79c82c4a2b256b93ecfbe5e13fcfda79dd6aba98
ls4050 <ls4050@naver.com>
2020-11-17 20:59:58 +0900

    1인칭추가+첫번째맵구현
    ㅇㅇ

---
+++ PearKkukBab/Assets/pickUp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pickUpp : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform theDest;
+
+        void OnMouseDown()
+        {
+            GetComponent<BoxCollider>().enabled = false;
+            GetComponent<Rigidbody>().useGravity = false;
+            this.transform.position = theDest.position;
+            this.transform.parent = GameObject.Find("Destination").transform;
+        }
+
+        void OnMouseUp()
+        {
+            this.transform.parent = null;
+            GetComponent<Rigidbody>().useGravity = true;
+            GetComponent<BoxCollider>().enabled = true;
+
+        }
+}
