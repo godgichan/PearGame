09651b9c0348b5d126c24dc68a2aa48cbfc87ab5
ls4050 <ls4050@naver.com>
2020-11-19 17:07:29 +0900

    베이스
    ㅇㅇ

---
+++ PearKkukBab/Assets/Script/DoorCallOpen.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DoorCallOpen : MonoBehaviour
+{
+
+    public float TheDistance;
+    public GameObject ActionDisplay;
+    public GameObject ActionText;
+    public GameObject TheDoor;
+    public AudioSource CreakSound;
+    
+    public GameObject ExtraCross;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        TheDistance = PlayerCasting.DistanceFromTarget;
+    }
+
+    void OnMouseOver () {
+        if(TheDistance <= 3) {
+            ExtraCross.SetActive(true);
+            ActionDisplay.SetActive(true);
+            ActionText.SetActive(true);
+        }
+        if (Input.GetButtonDown("Action")) {
+            if(TheDistance <=3) {
+                this.GetComponent<BoxCollider>().enabled = false;
+                ActionDisplay.SetActive(false);
+                ActionText.SetActive(false);
+                CreakSound.Play();
+                TheDoor.GetComponent<Animation>().Play("FirstRoomOutDoor");
+                
+            }
+        }
+    }
+
+    void OnMouseExit() {
+        ExtraCross.SetActive(false);
+        ActionDisplay.SetActive(false);
+        ActionText.SetActive(false);
+    }
+}
---
+++ PearKkukBab/Assets/Script/Sequences/AOpening.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
+using UnityEngine.UI;
+public class AOpening : MonoBehaviour
+{
+    public GameObject ThePlyer;
+    public GameObject FadeScreenIn;
+    public GameObject TextBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ThePlyer.GetComponent<FirstPersonController>().enabled = false;
+        StartCoroutine(ScenePlayer());
+    }
+
+    IEnumerator ScenePlayer() {
+        yield return new WaitForSeconds(1f);
+        FadeScreenIn.SetActive(false);
+        TextBox.GetComponent<Text>().text = "으... 머리 아파 \n 여기가 어디지 ?";
+        yield return new WaitForSeconds(2);
+        TextBox.GetComponent<Text>().text = "";
+        ThePlyer.GetComponent<FirstPersonController>().enabled = true;
+    }
+}
