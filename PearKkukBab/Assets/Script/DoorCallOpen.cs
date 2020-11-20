961d90f088a656c10edc18dd0533aa0f4b9461f7
ls4050 <ls4050@naver.com>
2020-11-20 23:18:14 +0900

    최신베이스 좋버그편
    ㄱ

--- PearKkukBab/Assets/Script/DoorCallOpen.cs
+++ PearKkukBab/Assets/Script/DoorCallOpen.cs
@@ -5,8 +5,11 @@ using UnityEngine.UI;
 
 public class DoorCallOpen : MonoBehaviour
 {
+    private bool _isInsideTrigger = false;
 
-    public float TheDistance;
+
+    
+    //public float TheDistance;
     public GameObject ActionDisplay;
     public GameObject ActionText;
     public GameObject TheDoor;
@@ -14,33 +17,67 @@ public class DoorCallOpen : MonoBehaviour
     
     public GameObject ExtraCross;
     
-    // Update is called once per frame
-    void Update()
-    {
-        TheDistance = PlayerCasting.DistanceFromTarget;
-    }
 
-    void OnMouseOver () {
-        if(TheDistance <= 3) {
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            _isInsideTrigger = true;
             ExtraCross.SetActive(true);
             ActionDisplay.SetActive(true);
             ActionText.SetActive(true);
         }
-        if (Input.GetButtonDown("Action")) {
-            if(TheDistance <=3) {
-                this.GetComponent<BoxCollider>().enabled = false;
-                ActionDisplay.SetActive(false);
-                ActionText.SetActive(false);
-                CreakSound.Play();
-                TheDoor.GetComponent<Animation>().Play("FirstRoomOutDoor");
-                
-            }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            _isInsideTrigger = false;
+            ActionDisplay.SetActive(false);
+            ActionText.SetActive(false);
+            ExtraCross.SetActive(false);
         }
     }
 
-    void OnMouseExit() {
-        ExtraCross.SetActive(false);
-        ActionDisplay.SetActive(false);
-        ActionText.SetActive(false);
+    
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(_isInsideTrigger)
+        {
+            if(Input.GetKeyDown(KeyCode.F))
+            {
+                CreakSound.Play();
+                TheDoor.GetComponent<Animation>().Play("FirstRoomOutDoor");
+            }
+        }
+        //TheDistance = PlayerCasting.DistanceFromTarget;
     }
+
+    // void OnMouseOver () {
+    //     if(TheDistance <= 3) {
+    //         ExtraCross.SetActive(true);
+    //         ActionDisplay.SetActive(true);
+    //         ActionText.SetActive(true);
+    //     }
+    //     if (Input.GetButtonDown("Action")) {
+    //         if(TheDistance <=3) {
+    //             this.GetComponent<BoxCollider>().enabled = false;
+    //             ActionDisplay.SetActive(false);
+    //             ActionText.SetActive(false);
+    //             CreakSound.Play();
+    //             TheDoor.GetComponent<Animation>().Play("FirstRoomOutDoor");
+                
+    //         }
+    //     }
+    // }
+
+    // void OnMouseExit() {
+    //     ExtraCross.SetActive(false);
+    //     ActionDisplay.SetActive(false);
+    //     ActionText.SetActive(false);
+    // }
 }
